1eddcb00a82e95a28e6e95ed044d1a41d9b44db1
CodeKei02 <keicode.dev02@gmail.com>
2025-08-22 11:51:52 -0400

    refactor: implementing a sidebar-links

---
+++ src/constants/links/sidebar-links.ts
@@ -0,0 +1,37 @@
+import { MuiIconName } from "@/components/ui/MuiIcon";
+
+export type SidebarLink = {
+  icon: MuiIconName;
+  label: string;
+  href: string;
+};
+
+const links: SidebarLink[] = [
+  {
+    icon: "Home",
+    label: "Inicio",
+    href: "/",
+  },
+  {
+    icon: "CalendarMonth",
+    label: "Calendario",
+    href: "/calendario",
+  },
+  {
+    icon: "Business",
+    label: "Consultorios",
+    href: "/consultorios",
+  },
+  {
+    icon: "PeopleAlt",
+    label: "Pacientes",
+    href: "/pacientes",
+  },
+  {
+    icon: "ContentPaste",
+    label: "Plantillas Episodios",
+    href: "/plantillas",
+  },
+];
+
+export { links };
