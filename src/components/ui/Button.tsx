9e27c80de9366724620ceceec93665f4d3d4c05f
CodeKei02 <keicode.dev02@gmail.com>
2025-08-22 12:16:39 -0400

    style: changing template string to quotes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Button } from "@mui/material";
 export const Btn: React.FC<{
   others?: string;
-  linkTo?: string;
+  linkTo?: string | { pathname: string; query: { id: string } };
   children: React.ReactNode | string;
   onClick?: () => void;
 }> = ({ others, children, linkTo, onClick }) => {
@@ -10,7 +10,7 @@ export const Btn: React.FC<{
     return (
       <Link
         href={linkTo}
-        className={`inline-flex items-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${others}`}
+        className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 text-black border border-gray-300 w-full xs:w-auto"
       >
         {children}
       </Link>
