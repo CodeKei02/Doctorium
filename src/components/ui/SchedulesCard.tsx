be2af3b3f2d52c4a09587f5a42cf706bea15570a
CodeKei02 <keicode.dev02@gmail.com>
2025-08-27 18:03:17 -0400

    feat: implement others styles

--- src/components/ui/SchedulesCard.tsx
+++ src/components/ui/SchedulesCard.tsx
@@ -2,15 +2,9 @@ import { useChecked } from "@/hooks/useChecked";
 import { Btn } from "./Button";
 import { Check } from "./Check";
 import { MuiIcon } from "./MuiIcon";
-// interface SchedulesCardProps {
-//   id: string;
-//   checked: boolean;
-//   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-//   day: string;
-//   startTime: string;
-//   endTime: string;
-// }
-
+import { CustomSelect } from "./CustomSelect";
+import { Form, Formik } from "formik";
+import { TimeSelect } from "./TimeSelect";
 const days = [
   "Lunes",
   "Martes",
@@ -21,15 +15,13 @@ const days = [
   "Domingo",
 ];
 
-// const hours = "00:00";
-
 export const SchedulesCard = () => {
   const { checked, handleChange } = useChecked();
-
+  const isChecking = (id: string) => checked[id] || false;
   return (
     <>
       {days.map((day, index) => (
-        <div key={index}>
+        <div key={index} className="border border-gray-200 rounded-md my-3 p-2">
           {/* Headline card */}
           <div className="flex justify-between">
             <div className="flex">
@@ -38,18 +30,36 @@ export const SchedulesCard = () => {
                 checked={checked[index.toString()] || false}
                 onChange={(e) => handleChange(index.toString(), e)}
               />
-              <p>{day}</p>
-              <p>{day.slice(0, 3)}</p>
+              <div className="flex self-center gap-2">
+                <p>{day}</p>
+                <p className="text-gray-400">{day.slice(0, 3).toUpperCase()}</p>
+              </div>
             </div>
-            <Btn>
+            <Btn others="w-50">
               <MuiIcon name="Add" />
               Agregar Horario
             </Btn>
           </div>
           {/* select time */}
-          <div className="flex items-center gap-2">
-            <p>De</p>
-          </div>
+          {isChecking(index.toString()) ? (
+            <div className="flex items-center gap-2">
+              <Formik
+                initialValues={{ startTime: "08:00", endTime: "17:00" }}
+                onSubmit={() => {}}
+              >
+                <Form className="w-full my-3 p-3 rounded-md flex items-center gap-2 bg-gray-200">
+                  <label>De</label>
+                  <TimeSelect name={`startTime_${index}`} />
+                  <label>a</label>
+                  <TimeSelect name={`endTime_${index}`} />
+                </Form>
+              </Formik>
+            </div>
+          ) : (
+            <div className="w-full my-3 p-3 rounded-md gap-2 bg-gray-200">
+              No disponible este día
+            </div>
+          )}
         </div>
       ))}
     </>
