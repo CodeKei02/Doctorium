84bb5c6a22814ac87684107982aa95a46f456564
CodeKei02 <keicode.dev02@gmail.com>
2025-09-14 18:59:36 -0400

    feat: improve ui

--- src/pages/calendario.tsx
+++ src/pages/calendario.tsx
@@ -1,10 +1,19 @@
 import DragAndDrop from "@/components/ui/DragAndDrop";
+import { Btn } from "@/components/ui/Button";
 
 const CalendarioPage = () => {
   return (
-    <div>
+    <>
+      <div className="flex justify-between w-[90%] mx-auto my-2">
+        <h2 className="mb-4 text-lg font-semibold text-gray-900">
+          Reprogramar citas
+        </h2>
+        {/* Implement a modal for adding new appointments */}
+        <Btn onClick={() => {}}>Agregar cita</Btn>
+      </div>
+
       <DragAndDrop />
-    </div>
+    </>
   );
 };
 
